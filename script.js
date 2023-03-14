3c0a72364c28b47001f9e04d16156c5ba46fadbe
NirajGadara <120002683+NirajGadara@users.noreply.github.com>
2023-03-14 14:27:23 +0530

    js code

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function myForm(){
     let BirthDate = document.getElementById("birthdate").value;
     let App_Time = document.getElementById("app_time").value;
     let favcolor = document.getElementById("favcolor").value;
-    let Cash = document.getElementById("cash").value;
-    let Card = document.getElementById("card").value;
-    let Upi = document.getElementById("upi").value; 
+    let Pyement = document.getElementsByName("pyement").value;
     let Age = document.getElementById("age").value;
 
 
@@ -21,10 +19,47 @@ function myForm(){
     let birthdate = localStorage.setItem("BirthDate",BirthDate);
     let app_time = localStorage.setItem("App_Time",App_Time);
     let favColor = localStorage.setItem("favcolor",favcolor);
-    let cash = localStorage.setItem("Cash",Cash);
-    let card = localStorage.setItem("Card",Card);
-    let upi = localStorage.setItem("Upi",Upi);
     let age = localStorage.setItem("Age",Age);
 
+    let ;
+    let i;
+    for(i=0; i<=Pyement.length; i++){
+        if(Pyement[i].checked){
+            let py = localStorage.setItem("Pyement",Pyement[i].value);
+        }
+    }
 
+}
+
+function valiData(){
+
+
+    if(firstname.value.trim()=="")
+    {
+        alert("First Name Is Blank");
+        firstname.style.border = "solid 1px aqua";
+        document.getElementById("invalid_fname").style.visibility="visible";
+        return false;
+    }
+    else if(lastname.value.trim()=="")
+    {
+        alert("Last Name Is Blank");
+        lastname.style.border = "solid 1px aqua";
+        document.getElementById("invalid_lname").style.visibility="visible";
+        return false;
+    }
+    else if(num.value.trim().length<10)
+    {
+        alert("Enter 10 Number");
+        num.style.border = "solid 1px aqua";
+        document.getElementById("invalid_num").style.visibility="visible";
+        return false;
+    }
+    else if(email.value.trim()=="")
+    {
+        alert("Email Is Blank");
+        email.style.border = "solid 1px aqua";
+        document.getElementById("invalid_email").style.visibility="visible";
+        return false;
+    }
 }
